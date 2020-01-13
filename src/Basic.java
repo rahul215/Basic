fa5344824fa767ec0b100ecc7000803abf319e97
rahul215 <rahul.saxena_cs18@gla.ac.in>
2020-01-13 22:46:59 +0530

    Initial commit

---
+++ src/Basic.java
@@ -0,0 +1,7 @@
+public class Basic {
+    public static void main(String[] args) {
+        double result;
+        result = ((25.5 * 3.5 - 3.5 * 3.5)/(40.5 - 4.5));
+        System.out.println(result);
+    }
+}
